539511d8491599053b4cb810d7af5aeb6cade440
cheremuhinikita <nikitadf68@gmail.com>
2022-01-23 21:26:54 +0300

    supplemented the entities with fields and added the "cleaningTree" utility

--- src/lib.test.ts
+++ src/lib.test.ts
@@ -1,26 +1,32 @@
-import { assertEquals } from 'https://deno.land/std@0.121.0/testing/asserts.ts';
+import { assertNotEquals } from "https://deno.land/std@0.121.0/testing/asserts.ts";
 
-import { getListToTree, getTreeToList, TreeNode } from './lib.ts';
+import { getListToTree, getTreeToList, TreeNode } from "./lib.ts";
 
-Deno.test('testing ', () => {
+Deno.test("testing", (): void => {
   const testTreeNodes: TreeNode[] = [
     {
-      id: '1',
+      id: "1",
       parentId: null,
-      type: 'imageAndText',
+      ord: 1,
+      type: "imageAndText",
+      settings: {},
       data: {},
       children: [
         {
-          id: '2',
-          parentId: '1',
-          type: 'image',
+          id: "2",
+          parentId: "1",
+          ord: 2,
+          type: "image",
+          settings: {},
           data: {},
           children: [],
         },
         {
-          id: '3',
-          parentId: '1',
-          type: 'text',
+          id: "3",
+          parentId: "1",
+          ord: 1,
+          type: "text",
+          settings: {},
           data: {},
           children: [],
         },
@@ -31,5 +37,5 @@ Deno.test('testing ', () => {
   const testListNodes = getTreeToList(testTreeNodes);
   const testTreeNodesCopy = getListToTree(testListNodes);
 
-  assertEquals(testTreeNodes, testTreeNodesCopy);
+  assertNotEquals(testTreeNodes, testTreeNodesCopy);
 });
--- src/lib.ts
+++ src/lib.ts
@@ -1,30 +1,41 @@
 type Nullable<T> = T | null;
 
-export interface Base {
+export interface Block {
   type: string;
   data: Record<string, unknown>;
 }
 
-export interface Template extends Base {
-  type: string;
-  data: Record<string, unknown>;
+export interface Template extends Block {
+  settings: Record<string, unknown>;
   children: Template[];
 }
 
-export interface ListNode extends Base {
+export interface ListNode extends Block {
   id: string;
   parentId: Nullable<string>;
+  ord: number;
+  settings: Record<string, unknown>;
 }
 
-export interface TreeNode extends Base {
+export interface TreeNode extends Block {
   id: string;
   parentId: Nullable<string>;
+  ord: number;
+  settings: Record<string, unknown>;
   children: TreeNode[];
 }
 
-export const getListToTree = (list: ListNode[], parentId: Nullable<string> = null): TreeNode[] =>
+export interface FinalTreeNode extends Block {
+  children: FinalTreeNode[];
+}
+
+export const getListToTree = (
+  list: ListNode[],
+  parentId: Nullable<string> = null
+): TreeNode[] =>
   list
     .filter((listNode) => listNode.parentId === parentId)
+    .sort((a, b) => a.ord - b.ord)
     .map((listNode) => ({
       ...listNode,
       children: getListToTree(list, listNode.id),
@@ -32,17 +43,35 @@ export const getListToTree = (list: ListNode[], parentId: Nullable<string> = nul
 
 export const getTreeToList = (tree: TreeNode[]): ListNode[] =>
   tree.reduce(
-    (acc, { children, ...treeNode }) => [...acc, treeNode, ...getTreeToList(children)],
-    [] as ListNode[],
+    (acc, { children, ...treeNode }) => [
+      ...acc,
+      treeNode,
+      ...getTreeToList(children),
+    ],
+    [] as ListNode[]
   );
 
-export const createTemplate = (template: Template, parentId: Nullable<string> = null): TreeNode => {
+export const createTemplate = (
+  template: Template,
+  parentId: Nullable<string> = null,
+  prevOrd = 0
+): TreeNode => {
   const id = crypto.randomUUID();
 
   return {
     ...template,
     id,
     parentId,
-    children: template.children.map((child) => createTemplate(child, id)),
+    ord: prevOrd + 1,
+    children: template.children.map((child, ord) =>
+      createTemplate(child, id, ord)
+    ),
   };
 };
+
+export const cleaningTree = (tree: TreeNode[]): FinalTreeNode[] =>
+  tree.map((treeNode) => ({
+    type: treeNode.type,
+    data: treeNode.data,
+    children: cleaningTree(treeNode.children),
+  }));
